b98e4d95527b361a4913612f63ab05d33b83cf9b
ivsharVincit <ivan.sharafanovich@vincit.fi>
2025-07-17 22:01:26 +0200

    Fix: added default route

--- src/main/java/com/azure/demo/DemoApplication.java
+++ src/main/java/com/azure/demo/DemoApplication.java
@@ -2,12 +2,18 @@ package com.azure.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String home() {
+        return "Привет Даша!";
+    }
 }
