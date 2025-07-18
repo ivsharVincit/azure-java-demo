fc2c8b11724216d3bbced48bd40e0262b1552838
ivsharVincit <ivan.sharafanovich@vincit.fi>
2025-07-18 15:49:54 +0200

    Add UI display of saved greetings

--- src/main/java/com/azure/demo/controller/WebController.java
+++ src/main/java/com/azure/demo/controller/WebController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -21,4 +24,18 @@ public class WebController {
         model.addAttribute("greetings", greetings);
         return "index"; // templates/index.html
     }
+
+    @GetMapping("/form")
+    public String showForm(Model model) {
+        List<Greeting> greetings = greetingRepository.findAll();
+        model.addAttribute("greetings", greetings);
+        return "form";
+    }
+
+    @PostMapping("/submit")
+    public String submitGreeting(@RequestParam String name, RedirectAttributes redirectAttributes) {
+        greetingRepository.save(new Greeting(name));
+        return "redirect:/form";
+    } 
 }
+
